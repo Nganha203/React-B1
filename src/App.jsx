10fe7fb2d45eccc5cd70d539527c6023b4d81769
Nganha203 <nguyenngocnganha203@gmail.com>
2023-07-02 09:51:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Banner from "./Components/Baitap/Banner"
+import Header from "./Components/Baitap/Header"
+import Item from "./Components/Baitap/Item"
+import Footer from "./Components/Baitap/Footer"
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Banner></Banner>
+      <Item></Item>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
